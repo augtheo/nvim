727234727c3c87dae009d253d7854d11df97412d
Augustine Theodore <augustine.theodore@proton.me>
2023-12-27 13:59:52 +0530

    chore: Fix formatting of keymap definitions

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -159,38 +159,55 @@ M.buffers = function()
   require("telescope.builtin").buffers(customDropDownOpts)
 end
 
+M.find_in_dirs = function()
+  require("telescope.builtin").find_files(
+    vim.tbl_deep_extend("force", customFilePickerOpts, { cwd = vim.fn.input "Directory: " })
+  )
+end
+
+M.find_files_with_word = function()
+  require("telescope.builtin").find_files(
+    vim.tbl_deep_extend("force", customFilePickerOpts, { search_file = vim.fn.expand "<cword>" })
+  )
+end
+
+M.grep = function()
+  require("telescope.builtin").grep_string(
+    vim.tbl_deep_extend("force", customGrepPickerOpts, { search = vim.fn.input "Search: " })
+  )
+end
 -- stylua: ignore start
 local keymap = vim.keymap.set
 
 
 -- find
-keymap("n", "<leader>fb", M.buffers, { desc = "Buffers" } )
-keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", { desc = "Commands" })
-keymap("n", "<leader>f:", "<cmd>Telescope command_history<cr>", { desc = "History" })
-keymap("n", "<leader>f\"", "<cmd>Telescope registers<cr>", { desc = "Registers" })
-keymap("n", "<leader>fd", function() require("telescope.builtin").find_files(vim.tbl_deep_extend("force", customFilePickerOpts, { cwd = vim.fn.input "Directory: " }) ) end, { desc = "Files in Directory" })
-keymap("n", "<leader>ff", M.find_files, { desc = "Files" })
-keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help Pages" })
-keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Key Maps" })
-keymap("n", "<leader>fM", "<cmd>Telescope man_pages<cr>", { desc = "Man Pages" })
-keymap("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "Marks" })
-keymap("n", "<leader>fO", "<cmd>Telescope vim_options<cr>", { desc = "Options" })
-keymap("n", "<leader>fo", M.oldfiles, { desc = "Recent" })
-keymap("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume" })
+keymap("n", "<leader>fb", M.buffers                                                                                                , { desc = "Buffers" } )
+keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>"                                                                            , { desc = "Commands" })
+keymap("n", "<leader>f:", "<cmd>Telescope command_history<cr>"                                                                     , { desc = "History" })
+keymap("n", "<leader>f\"", "<cmd>Telescope registers<cr>"                                                                          , { desc = "Registers" })
+keymap("n", "<leader>fd", M.find_in_dirs                                                                                           , { desc = "Files in Directory" })
+keymap("n", "<leader>ff", M.find_files                                                                                             , { desc = "Files" })
+keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>"                                                                           , { desc = "Help Pages" })
+keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>"                                                                             , { desc = "Key Maps" })
+keymap("n", "<leader>fM", "<cmd>Telescope man_pages<cr>"                                                                           , { desc = "Man Pages" })
+keymap("n", "<leader>fm", "<cmd>Telescope marks<cr>"                                                                               , { desc = "Marks" })
+keymap("n", "<leader>fO", "<cmd>Telescope vim_options<cr>"                                                                         , { desc = "Options" })
+keymap("n", "<leader>fo", M.oldfiles                                                                                               , { desc = "Recent" })
+keymap("n", "<leader>fr", "<cmd>Telescope resume<cr>"                                                                              , { desc = "Resume" })
 
 
 -- git
-keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "Commits" })
-keymap("n", "<leader>gC", "<cmd>Telescope git_bcommits<CR>", { desc = "Commits(Buffer)" })
-keymap("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", { desc = "Branches" })
-keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>", { desc = "Status" })
+keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>"                                                                         , { desc = "Commits" })
+keymap("n", "<leader>gC", "<cmd>Telescope git_bcommits<CR>"                                                                        , { desc = "Commits(Buffer)" })
+keymap("n", "<leader>gb", "<cmd>Telescope git_branches<CR>"                                                                        , { desc = "Branches" })
+keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>"                                                                          , { desc = "Status" })
 
 -- search
-keymap("n", "<leader>sg", function() require("telescope.builtin").live_grep(customGrepPickerOpts) end, { desc = "Live Grep" })
-keymap("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Buffer" })
-keymap("n", "<leader>s/", function() require("telescope.builtin").grep_string( vim.tbl_deep_extend("force", customGrepPickerOpts, { search = vim.fn.input "Search: " })) end, { desc = "Grep" })
-keymap("n", "<leader>sw", function() require("telescope.builtin").grep_string(customGrepPickerOpts) end, { desc = "Cursor Word" })
-keymap("n", "<leader>sf", function() require("telescope.builtin").find_files(vim.tbl_deep_extend("force", customFilePickerOpts , {search_file = vim.fn.expand("<cword>")})) end, { desc = "Cursor Files" })
+keymap("n", "<leader>sg", function() require("telescope.builtin").live_grep(customGrepPickerOpts) end                              , { desc = "Live Grep" })
+keymap("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>"                                                           , { desc = "Buffer" })
+keymap("n", "<leader>s/", M.grep                                                                                                   , { desc = "Grep" })
+keymap("n", "<leader>sw", function() require("telescope.builtin").grep_string(customGrepPickerOpts) end                            , { desc = "Cursor Word" })
+keymap("n", "<leader>sf", M.find_files_with_word                                                                                   , { desc = "Cursor Files" })
 
 -- stylua: ignore end
 
